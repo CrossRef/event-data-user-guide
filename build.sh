f4958121562b28e0982e4779ddbb4664a75f76ae
Joe Wass <jwass@crossref.org>
2016-03-31 12:20:23 +0100

    Build script creates dated copy plus current.

--- build.sh
+++ build.sh
@@ -4,5 +4,9 @@ DATE=`date "+%Y-%m-%d-%H-%M"`
 
 dot -Tpng pushpull.dot  > build/pushpull.png
 
-pandoc -s event-data-manual.md --toc -o build/event-data-manual.$DATE.pdf
-pandoc -s event-data-manual.md --toc -o build/event-data-manual.$DATE.html
+rm build/event-data-manual.pdf
+
+pandoc -s event-data-manual.md --toc -o build/event-data-manual.pdf
+pandoc -s event-data-manual.md --toc -o build/event-data-manual.html
+
+cp build/event-data-manual.pdf build/event-data-manual.$DATE.pdf
