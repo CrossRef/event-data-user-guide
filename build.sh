7f74c5f9907c013588bb8ff4c0d88b1b953aa244
Joe Wass <jwass@crossref.org>
2016-04-19 13:08:41 +0100

    Build script 0.2

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ rm build/event-data-manual-future.md
 
 rm build/index.html
 
-gpp event-data-manual.md -o build/event-data-manual-current.md -D__VERSION__="Version 0.1 MVP $DATE"
+gpp event-data-manual.md -o build/event-data-manual-current.md -D__VERSION__="Version 0.2 MVP $DATE"
 gpp event-data-manual.md -o build/event-data-manual-todo.md -D__TODO__=true -D__VERSION__="Version 0.1 MVP Draft with TODOs"
 gpp event-data-manual.md -o build/event-data-manual-future.md -D__FUTURE__=true -D__TODO__=true -D__VERSION__="Version 0.1 Draft MVP Pre-draft with Future Features and TODOs"
 
